e5f6c4236e68f07ca71c906159a8193bd0f9fb51
Dmitriy Rybakov <theimdim@gmail.com>
2019-10-09 20:25:50 +0400

    добавил интерактивности

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
-import 'bootstrap';
+//import 'bootstrap';
+
+//import player from "./video.js";
+
+$(function() {
+    $('.video-content').on('click', function(evt) {
+        $(this).addClass('is-playing');
+
+        $("#video")[0].src += "&autoplay=1";
+        evt.preventDefault();
+    });
+});
